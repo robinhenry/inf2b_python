9816e5a4dce0d5ab8d8834a699bd5b30c21e2b6f
Robin Henry <robin.henry012@gmail.com>
2018-05-10 15:55:08 +0100

    bnb_classify working

--- Task2/my_bnb_classify.py
+++ Task2/my_bnb_classify.py
@@ -19,20 +19,20 @@ def my_bnb_classify(Xtrn, Ctrn, Xtst, threshold):
     Xtst_bin = Xtst >= threshold
 
     # Matrices to store characteristics of each class
-    trn_zeros = np.zeros(C, D)          # C-by-D matrix, numbers of 0's for each pixel
-    trn_ones = np.zeros(C, D)           # C-by_D matrix, numbers of 1's for each pixel
+    trn_zeros = np.zeros((C, D))          # C-by-D matrix, numbers of 0's for each pixel
+    trn_ones = np.zeros((C, D))           # C-by_D matrix, numbers of 1's for each pixel
 
     # Initialise matrix to store likelihoods
-    likelihoods = np.zeros(N, C)  
+    likelihoods = np.zeros((N, C))  
 
     for k in range(0, C):
         # Select training samples from class k and 1 row in case there is only 1 training sample (just in case)
-        Xtrn_k = np.concatenate([Xtrn_bin[Ctrn == k, :]], np.zeros(1,D))
+        Xtrn_k = np.concatenate((Xtrn_bin[(Ctrn == k)[:,0], :], np.zeros((1,D))))
         # Number of samples of class k
-        nbr_samples_k = Xtrn_k.shape[0] - 1
+        nbr_samples_k = Xtrn_k.shape[0]
+        nbr_samples_k = Xtrn_k.shape[0] 
         # P(D_i = 0|C_k), where D_i is the ith element of a feature vector D
-        #   + using Laplace's rule to avoid 0's
-        trn_zeros[k,:] = np.sum(Xtrn_k == 0, axis=1) / nbr_samples_k
+        trn_zeros[k,:] = np.sum(Xtrn_k == 0, axis=0) / nbr_samples_k
 
         # Compute the likelihoods
         factor_0 = np.power(trn_zeros[k,:], (1 - Xtst_bin))
